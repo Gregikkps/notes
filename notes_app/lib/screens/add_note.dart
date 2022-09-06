72b3b8209c9c784314f1278e35757742dfccafd0
gregi <gregikkps@gmail.com>
2022-09-06 23:19:26 +0200

    TextFormField

--- notes_app/lib/screens/add_note.dart
+++ notes_app/lib/screens/add_note.dart
@@ -10,12 +10,29 @@ class AddNoteScreen extends StatefulWidget {
 }
 
 class _AddNoteScreenState extends State<AddNoteScreen> {
+  final noteController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
         centerTitle: true,
+        actions: [
+          TextButton(
+            child: Text('Save'),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Center(
+        child: TextFormField(
+          controller: noteController,
+          minLines: 3,
+          maxLines: 10,
+          decoration: const InputDecoration(
+            labelText: 'Note ...',
+          ),
+        ),
       ),
     );
   }
